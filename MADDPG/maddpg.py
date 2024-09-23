93dbbe0e4b29d43fd131b371f23e323007347b48
HRIDAY MEHTA <hridaym.211me129@nitk.edu.in>
2024-09-23 19:58:33 +0530

    Changes to the MADDPG algo, with wandb tracking

--- MADDPG/maddpg.py
+++ MADDPG/maddpg.py
@@ -3,6 +3,8 @@ from collections import namedtuple, deque
 import lbforaging
 import tqdm
 import numpy as np
+import wandb
+from tqdm import tqdm
 import torch
 import torch.nn as nn 
 import torch.optim as optim
@@ -11,6 +13,8 @@ import gymnasium as gym
 
 
 Experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+
+wandb.init(project="maddpg-foraging", name = "MADDPG")
 
 class ReplayBuffer:
     def __init__(self, capacity=10000):
@@ -47,12 +51,13 @@ class Actor(nn.Module):
 class Critic(nn.Module):
     def __init__(self, state_dim, action_dim, n_agents):
         super(Critic, self).__init__()
-        self.fc1 = nn.Linear(state_dim * n_agents + action_dim * n_agents, 64)
+        self.fc1 = nn.Linear((state_dim + action_dim) * n_agents , 64)
         self.fc2 = nn.Linear(64, 32)
         self.fc3 = nn.Linear(32, 1)
         
     def forward(self, state, action):
-        x = torch.cat([state, action], dim=1)
+        # print(state.shape, action.shape)
+        x = torch.cat((state, action.squeeze(1)), dim=1)
         x = torch.relu(self.fc1(x))
         x = torch.relu(self.fc2(x))
         return self.fc3(x)
@@ -71,7 +76,6 @@ class MADDPG:
         self.actor_optimizers = [optim.Adam(actor.parameters(), lr=lr) for actor in self.actors]
         self.critic_optimizers = [optim.Adam(critic.parameters(), lr=lr) for critic in self.critics]
         
-        # Initialize target networks
         for actor, actor_target in zip(self.actors, self.actors_target):
             self.soft_update(actor, actor_target, 1.0)
         for critic, critic_target in zip(self.critics, self.critics_target):
@@ -81,7 +85,6 @@ class MADDPG:
         actions = []
         for i, actor in enumerate(self.actors):
             state_array = self.obs_to_array(obs, i)
-
             state = torch.FloatTensor(state_array).unsqueeze(0) 
             action_probs = actor(state).squeeze().detach().numpy()  
 
@@ -105,93 +108,126 @@ class MADDPG:
             dict_obs['food'].flatten(),
             [dict_obs['hunger']]
         ])
+        
+    def ProcessMemories(self, memories, num_agents, batch_size):
+        all_states = []
+        all_actions = []
+        all_rewards = []
+        all_next_states = []
+        all_target_actions = []
+        all_dones = []
+
+        for i in range(num_agents): 
+            states_i, actions_i, rewards_i, next_states_i, dones_i = memories[i].sample(batch_size)
+            actions_one_hot_i = F.one_hot(actions_i.long(), num_classes=6)
+            target_actions_i = self.actors_target[i](next_states_i)
+            target_actions_one_hot_i = F.one_hot(target_actions_i.argmax(dim=1), num_classes=6)
+            
+            all_states.append(states_i)
+            all_actions.append(actions_one_hot_i)
+            all_rewards.append(rewards_i)
+            all_next_states.append(next_states_i)
+            all_target_actions.append(target_actions_one_hot_i)
+            all_dones.append(dones_i)
+
+        states = torch.cat(all_states, dim=1)
+        actions = torch.cat(all_actions, dim=1)
+        rewards = torch.cat(all_rewards, dim=1)
+        next_states = torch.cat(all_next_states, dim=1)
+        target_actions = torch.cat(all_target_actions, dim=1)
+        dones = torch.cat(all_dones, dim=1)
+        
+        return states, actions, rewards, next_states, target_actions, dones
     
     def update(self, memories, batch_size):
         for i in range(self.n_agents):
             if len(memories[i]) < batch_size:
                 return
             
+            combinedStates, combinedActions, _,  combinedNextStates, combinedTargetActions, _ = self.ProcessMemories(memories, self.n_agents, batch_size)
+            # print("HOPEFULLY WORKS")
+            # print(combinedStates.shape, combinedActions.shape, combinedNextStates.shape, combinedTargetActions.shape)
+            
+            combinedActions = combinedActions.view(batch_size, -1)
+            
             states, actions, rewards, next_states, dones = memories[i].sample(batch_size)
-            print("CHECK2")
-            actions_one_hot = torch.zeros(batch_size, self.n_agents, actions.shape[-1])
-            for j in range(self.n_agents):
-                actions_one_hot[:, j, actions[:, j].long()] = 1
+            
+            actions_one_hot = F.one_hot(actions.long(), num_classes=6)
+            
+            target_actions = self.actors_target[i](next_states)
 
-            target_actions = torch.zeros_like(actions_one_hot)
-            for j, actor_target in enumerate(self.actors_target):
-                target_actions[:, j] = actor_target(next_states[:, j*states.shape[1]:(j+1)*states.shape[1]])
+            target_q = rewards + self.gamma * self.critics_target[i](combinedNextStates, combinedTargetActions) * (1 - dones)
+
+            current_q = self.critics[i](combinedStates, combinedActions) #combined actions is a one hot vector
+
+            critic_loss = F.mse_loss(current_q, target_q.detach())
             
-            target_critic_input = torch.cat((next_states, target_actions.view(batch_size, -1)), dim=1)
-            current_critic_input = torch.cat((states, actions_one_hot.view(batch_size, -1)), dim=1)
-            
-            target_q = rewards[:, i] + self.gamma * self.critics_target[i](target_critic_input) * (1 - dones[:, i])
-            current_q = self.critics[i](current_critic_input)
-            
-            critic_loss = nn.MSELoss()(current_q, target_q.detach())
             self.critic_optimizers[i].zero_grad()
             critic_loss.backward()
             self.critic_optimizers[i].step()
+            
+            actor_loss = -self.critics[i](combinedStates, combinedActions).mean()
 
-            pred_actions = torch.zeros_like(actions_one_hot)
-            for j in range(self.n_agents):
-                if j == i:
-                    pred_actions[:, j] = self.actors[j](states[:, j*states.shape[1]:(j+1)*states.shape[1]])
-                else:
-                    pred_actions[:, j] = self.actors[j](states[:, j*states.shape[1]:(j+1)*states.shape[1]]).detach()
-            
-            actor_loss = -self.critics[i](torch.cat((states, pred_actions.view(batch_size, -1)), dim=1)).mean()
-            
             self.actor_optimizers[i].zero_grad()
             actor_loss.backward()
             self.actor_optimizers[i].step()
             
             self.soft_update(self.critics[i], self.critics_target[i], self.tau)
             self.soft_update(self.actors[i], self.actors_target[i], self.tau)
+
     
     def soft_update(self, local_model, target_model, tau):
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
             target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
 
-def train_maddpg(env, n_episodes=10000, max_steps=100, batch_size=64):
+def train_maddpg(env, n_episodes=10000, max_steps=1000, batch_size=64):
     n_agents = env.n_agents
     sample_obs = env.reset()
     state_dim = sample_obs[0][0].shape[0] 
     action_dim = env.action_space[0].n
     
     maddpg = MADDPG(state_dim, action_dim, n_agents)
-    memories = [ReplayBuffer() for _ in range(n_agents)]
+    memories = [ReplayBuffer() for _ in range(n_agents)] #Separate RBs? Will need to verify this.
     
-    for episode in range(n_episodes):
+    for episode in tqdm(range(n_episodes), desc="Training", leave = True):
         obs = env.reset()
         episode_rewards = [0 for _ in range(n_agents)]
         # print("The obs is ", obs)
         for step in range(max_steps):
-            print(f"Episode : {episode}, Step : {step}")
+            # print(f"Episode : {episode}, Step : {step}")
             actions = maddpg.select_action(obs)
-            print("THE ACTIONS ARE ", actions)
             next_obs, rewards, dones, _ , _ = env.step(actions)
-
+            next_obs = ((next_obs), {})
             
             if isinstance(dones, bool):  
                 dones = [dones] * n_agents
             
+            if dones == True: 
+                dones = [True] * n_agents
+                
+            
             for i in range(n_agents):
                 state_array = maddpg.obs_to_array(obs, i)
                 next_state_array = maddpg.obs_to_array(next_obs, i)
-                print("CHECK1")
+                # print("ACTIONS ", actions[i])
+                # print("REWARDS ", rewards[i])
+                # try:
+                #     print("DONES ", dones[i])
+                # except:
+                #     print("DONES ", dones)
                 memories[i].add(state_array, actions[i], rewards[i], next_state_array, dones[i])
                 episode_rewards[i] += rewards[i]
             
-            obs = ((next_obs), {})
-
+            obs = next_obs
             
             maddpg.update(memories, batch_size)
             
             if all(dones):
                 break
         
-        if episode % 100 == 0:
-            print(f"Episode {episode}, Average Rewards: {np.mean(episode_rewards)}")
+        # if episode % 1 == 0:
+        #     print(f"Episode {episode}, Average Rewards: {np.mean(episode_rewards)}")
+        wandb.log({"Average Reward": np.mean(episode_rewards)}, step = episode)
     
     return maddpg
 
